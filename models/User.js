310990133f0cde71b362828b74a3d52c67e9f3a5
TheRedQueen <pand3m1x@gmail.com>
2026-03-27 12:03:33 -0400

    ☘︎ ݁:࿔ Added Hashbrowns to password in User ☘︎ ݁:࿔

--- models/User.js
+++ models/User.js
@@ -14,4 +14,17 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+// hash user password
+userSchema.pre("save", async function () {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 export default mongoose.model('User', userSchema);
