8ff0c6e2ef6aa9e4635a6be12c57ba94de70d87b
TheRedQueen <pand3m1x@gmail.com>
2026-03-27 19:53:55 -0400

    ☘︎ ݁:࿔  Added route successfully, and added a project into database ☘︎ ݁:࿔

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -6,4 +6,24 @@ const router = express.Router();
 
 router.use(authMiddleware)
 
+
+// POST /api/projects creating projects (similiar to notes/labs?)
+
+router.post('/', async (req,res) => {
+  try{
+    const project = await Project.create({
+    ...req.body,
+    user:req.user._id
+
+    });
+
+    res.json(project);
+    console.log("Ayyeee! Project added!", project)
+  } catch(err) {
+
+    res.json(500).json(err);
+    console.log(err.message,"Nothing was made")
+  }
+});
+
 export default router;
