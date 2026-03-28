a7cb1690005e6d09840d1b3ee5c038f94fe65acd
TheRedQueen <pand3m1x@gmail.com>
2026-03-27 20:46:57 -0400

    ☘︎ ݁:࿔ added get route by :id and it works! ☘︎ ݁:࿔

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -10,7 +10,9 @@ router.use(authMiddleware)
 // POST /api/projects creating projects (similiar to notes/labs?)
 
 router.post('/', async (req,res) => {
+
   try{
+
     const project = await Project.create({
     ...req.body,
     user:req.user._id
@@ -19,28 +21,51 @@ router.post('/', async (req,res) => {
 
     res.json(project);
     console.log("Ayyeee! Project added!", project)
+
   } catch(err) {
 
     res.json(500).json(err);
     console.log(err.message,"Nothing was made")
+
   }
 });
 
 // GET /api/projects
 router.get('/', async (req,res) => {
+
   try{
+
     const projects = await Project.find({})
 
     res.json(projects);
     console.log("Here are the projects!", projects)
+
   } catch(err) {
 
     res.json(500).json(err);
     console.log(err.message,"Projects seem to be missing")
+
   }
 });
 
 // GET /api/projects/:id 
+
+router.get('/:id', async (req,res) => {
+  
+  try{
+
+    const project = await Project.findById(req.params.id)
+
+    res.json(project);
+    console.log("Here is the project!", project)
+
+  } catch(err) {
+
+    res.json(500).json(err);
+    console.log(err.message,"Project not found")
+
+  }
+});
 
 // PUT /api/projects/:id
 
